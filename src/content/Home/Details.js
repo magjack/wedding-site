e9515fdce47924e3f6bd4af692184ec53c25b7db
Jack <magjack94@gmail.com>
2024-04-11 23:59:45 +0100

    Evening guests only

--- src/content/Home/Details.js
+++ src/content/Home/Details.js
@@ -42,7 +42,7 @@ export default {
                 <hr/>
                 <br/>
                 <br/>
-                <h3>Please RSVP for reception by May 1<sup>st</sup></h3>
+                <h3>Please RSVP for reception (Evening guests only) by May 1<sup>st</sup></h3>
                 <a href="https://forms.gle/1AVf5o2n3ciem7kg6" className="btn btn-primary" role="button">
                         RSVP Reception
                 </a>
